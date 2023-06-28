c1dcb83a1d71155b32fe6a69b755474239809e97
yonathang-mgnd <yonathang@magnadint.com>
2023-06-28 12:11:20 +0300

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormContainerComponent } from './form-container/form-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormContainerComponent
   ],
   imports: [
     BrowserModule
